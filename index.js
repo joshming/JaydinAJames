83dbca7880bb67ef2ae97e7866ae77c6e23d48fb
Joshua Ming <60520554+joshming@users.noreply.github.com>
2022-01-30 17:04:23 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// document.getElementById('Yes').addEventListener('click', yesClick);
+// document.getElementById('No').addEventListener('click', noClick);
+//
+// function yesClick() {
+//   console.log('clicked');
+//   window.location.href = '/goodJob.html';
+// }
+//
+// function noClick() {
+//   console.log('clicked');
+//   window.location.href = 'goodJob.html';
+// }
